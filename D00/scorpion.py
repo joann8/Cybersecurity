ff73640bd40668e375a49e312dfe3bd534b88647
jacher <joann.acher@protonmail.com>
2024-10-31 13:27:28 +0100

    push

--- D00/scorpion.py
+++ D00/scorpion.py
@@ -15,45 +15,50 @@ def get_file_creation_date(path):
     img_info = os.stat(path)
     creation_time = img_info.st_ctime
     # Date de création en secondes depuis l'époque
-    print(f"  --> DateTimeCreation: {time.ctime(creation_time)}")
+    print(f"    --> DateTimeCreation: {time.ctime(creation_time)}")
 
 
-def print_img_info(img):
+def print_img_info(img, path, DateTimeOriginal):
     """Print basic img attributes"""
-    print("\n---> Basic attributes")
-    print(f"  --> Format: {img.format}\n  --> Mode: {img.mode}\n"
-          f"  --> Size: {img.size}\n  --> Palette: {img.palette}\n")
+    print("> Basic attributes")
+    print(f"    --> Format: {img.format}\n    --> Mode: {img.mode}\n"
+          f"    --> Size: {img.size}\n    --> Palette: {img.palette}")
+    if not DateTimeOriginal:
+        get_file_creation_date(path)
 
 
-def print_img_exif(img, path):
+def print_img_exif(img):
     """Print EXIF data for img"""
     # EXIF (Exchangeable Image File Format)
     exif_data = img._getexif()
     DateTimeOriginal = False
     if exif_data:
-        print("---> Exif data")
+        print("> EXIF data")
         for tag, value in exif_data.items():
             # Convertir le tag en nom lisible
             tag_name = ExifTags.TAGS.get(tag, tag)
-            print(f"  --> {tag_name}: {value}")
+            print(f"    --> {tag_name}: {value}")
             if tag_name == "DateTimeOriginal":
                 DateTimeOriginal = True
-        if not DateTimeOriginal:
-            get_file_creation_date(path)
-        print("\n")
     else:
-        print("---> No EXIF data found <---")
+        print("> No EXIF data found")
+    return DateTimeOriginal
 
 
 def get_img_data(path):
     """Get the data from an img"""
     if not is_image_file(path):
-        print(f"!! '{path}' not an image\n")
+        print(f"*** ! '{path}' not a valid image (check extension) ***")
         return
-    img = Image.open(path)
-    print(f"______Data for '{path}'_________")
-    print_img_info(img)
-    print_img_exif(img, path)
+    try:
+        img = Image.open(path)
+        print(f"*** Data for '{path}' ***")
+        DateTimeOriginal = print_img_exif(img)
+        print_img_info(img, path, DateTimeOriginal)
+        print("\n")
+
+    except Exception:
+        print(f"*** ! Error opening '{path} (check path) ***")
 
 
 def main():
